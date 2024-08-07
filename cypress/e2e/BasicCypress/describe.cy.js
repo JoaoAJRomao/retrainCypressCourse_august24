f7824a97456fbfb1b00175231fd2267388a74996
Joao Romao <tupi_1800@hotmail.com>
2024-08-07 15:11:13 -0300

    Classroom about describe/it

---
+++ cypress/e2e/BasicCypress/describe.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it.skip("A external test...", () => {});
+
+describe("Should group tests...", () => {
+  describe("Should group more specific tests...", () => {
+    it("A specific test...", () => {});
+  });
+
+  describe("Should group more specific test 2...", () => {
+    it("A specific test 2...", () => {});
+  });
+
+  it.skip("A internal test...", () => {});
+});
