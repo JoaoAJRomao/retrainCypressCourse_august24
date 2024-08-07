be694aeaa7dbbc1c6da08f7217c5824d673c0c58
Joao Romao <tupi_1800@hotmail.com>
2024-08-07 17:02:56 -0300

    Classroom about debug

--- cypress/e2e/BasicCypress/basic.cy.js
+++ cypress/e2e/BasicCypress/basic.cy.js
@@ -8,15 +8,16 @@ describe("Cypress basic", () => {
 
   it("Should visit a page and assert a title", () => {
     cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+    cy.pause();
     cy.title().should("be.equal", "Campo de Treinamento");
-    cy.title().should("contain", "Campo");
+    cy.title().debug().should("contain", "Campo");
 
     cy.title()
       .should("be.equal", "Campo de Treinamento")
       .and("contain", "Campo");
   });
 
-  it.only("Should find and interact with an element", () => {
+  it("Should find and interact with an element", () => {
     cy.visit("https://www.wcaquino.me/cypress/componentes.html");
     cy.get("#buttonSimple").click().should("have.value", "Obrigado!");
   });
