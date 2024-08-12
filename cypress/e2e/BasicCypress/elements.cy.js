c73ba2bd653b0fb7cad8ae70b2adc2962563bd53
Joao Romao <tupi_1800@hotmail.com>
2024-08-12 20:19:48 -0300

    Classroom about links

--- cypress/e2e/BasicCypress/elements.cy.js
+++ cypress/e2e/BasicCypress/elements.cy.js
@@ -7,7 +7,22 @@ describe("Work with basic elements", () => {
     // cy.get("body").should("have.text", "Cuidado");
     cy.get("span").should("contain", "Cuidado");
     // cy.get("div").should("contain", "Cuidado");
-    cy.get('.facilAchar').should("contain", "Cuidado");
-    cy.get('.facilAchar').should("have.text", "Cuidado onde clica, muitas armadilhas...");
+    cy.get(".facilAchar").should("contain", "Cuidado");
+    cy.get(".facilAchar").should(
+      "have.text",
+      "Cuidado onde clica, muitas armadilhas..."
+    );
+  });
+
+  it.only("Links", () => {
+    cy.visit("https://www.wcaquino.me/cypress/componentes.html");
+    // cy.get("a").click();
+    cy.get('[href="#"]').click();
+    cy.get("#resultado").should("have.text", "Voltou!");
+
+    cy.reload()
+    cy.get("#resultado").should("not.have.text", "Voltou!");
+    cy.contains("Voltar").click();
+    cy.get("#resultado").should("have.text", "Voltou!");
   });
 });
