35705ba987f9fe43fd050286342ca46fe7f73cea
Joao Romao <tupi_1800@hotmail.com>
2024-08-13 16:03:00 -0300

    Classriim about radio button

--- cypress/e2e/BasicCypress/elements.cy.js
+++ cypress/e2e/BasicCypress/elements.cy.js
@@ -27,7 +27,7 @@ describe("Work with basic elements", () => {
     cy.get("#resultado").should("have.text", "Voltou!");
   });
 
-  it.only("TextFields", () => {
+  it("TextFields", () => {
     cy.get("#formNome").type("Cypress Test");
     cy.get("#formNome").should("have.value", "Cypress Test");
 
@@ -47,5 +47,11 @@ describe("Work with basic elements", () => {
       .clear()
       .type("Error{selectall}acerto", { delay: 100 })
       .should("have.value", "acerto");
+  });
+
+  it("RadioButton", () => {
+    cy.get("#formSexoFem").click().should("be.checked");
+    cy.get("#formSexoMasc").should("not.be.checked");
+    cy.get("[name=formSexo]").should("have.length", 2);
   });
 });
