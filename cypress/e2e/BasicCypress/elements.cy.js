1bf4aaabed077363afd79fe120fe338efba081e2
Joao Romao <tupi_1800@hotmail.com>
2024-08-12 21:40:33 -0300

    Classroom about textfields

--- cypress/e2e/BasicCypress/elements.cy.js
+++ cypress/e2e/BasicCypress/elements.cy.js
@@ -27,10 +27,25 @@ describe("Work with basic elements", () => {
     cy.get("#resultado").should("have.text", "Voltou!");
   });
 
-  describe("Work with basic elements", () => {
-    before(() => {
-      cy.visit("https://www.wcaquino.me/cypress/frame.html");
-    });
-    it("Extern", () => {});
+  it.only("TextFields", () => {
+    cy.get("#formNome").type("Cypress Test");
+    cy.get("#formNome").should("have.value", "Cypress Test");
+
+    cy.get("#elementosForm\\:sugestoes")
+      .type("textarea")
+      .should("have.value", "textarea");
+
+    cy.get(
+      "#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input"
+    ).type("???");
+
+    cy.get('[data-cy="dataSobrenome"]')
+      .type("Teste12345{backspace}{backspace}")
+      .should("have.value", "Teste123");
+
+    cy.get("#elementosForm\\:sugestoes")
+      .clear()
+      .type("Error{selectall}acerto", { delay: 100 })
+      .should("have.value", "acerto");
   });
 });
