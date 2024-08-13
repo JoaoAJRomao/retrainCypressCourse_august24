b6e481d2e5233dd6fe7c1acb6338581d910c5c1f
Joao Romao <tupi_1800@hotmail.com>
2024-08-13 17:15:53 -0300

    Classroom about retrys

--- cypress/e2e/BasicCypress/sync.cy.js
+++ cypress/e2e/BasicCypress/sync.cy.js
@@ -5,11 +5,18 @@ describe("Waits...", () => {
     cy.visit("https://www.wcaquino.me/cypress/componentes.html");
   });
 
-  it.only("Must wait available element", () => {
+  it("Must wait available element", () => {
     cy.get("#novoCampo").should("not.exist");
     cy.get("#buttonDelay").click();
     cy.get("#novoCampo").should("not.exist");
     cy.get("#novoCampo").should("exist");
     cy.get("#novoCampo").type("it work");
+  });
+
+  it("Must retry", () => {
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#buttonDelay").click();
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#novoCampo").should("exist").type("it work");
   });
 });
