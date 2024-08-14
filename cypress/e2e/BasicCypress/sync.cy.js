d2d38040dd8f57092725d1569bd994032c6cb4ba
Joao Romao <tupi_1800@hotmail.com>
2024-08-14 17:59:37 -0300

    Classroom about wait and timeout

--- cypress/e2e/BasicCypress/sync.cy.js
+++ cypress/e2e/BasicCypress/sync.cy.js
@@ -20,9 +20,23 @@ describe("Waits...", () => {
     cy.get("#novoCampo").should("exist").type("it work");
   });
 
-  it.only("Find feature use", () => {
+  it("Find feature use", () => {
     cy.get("#buttonList").click();
     cy.get("#lista li").find("span").should("contain", "Item 1");
     cy.get("#lista li span").should("contain", "Item 2");
+  });
+
+  it("Timeout use", () => {
+    // cy.get("#buttonDelay").click();
+    // cy.get("#novoCampo", { timeout: 1000 }).should("exist");
+
+    // cy.get("#buttonListDOM").click();
+    // cy.wait(5000);
+    // cy.get("#lista li span", { timeout: 30000 }).should("contain", "Item 2");
+
+    cy.get("#buttonListDOM").click();
+    cy.get("#lista li span").should("have.length", 1);
+
+    cy.get("#lista li span").should("have.length", 2);
   });
 });
